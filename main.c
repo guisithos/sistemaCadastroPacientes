dc9eb6bf01efea289f784c2088217c71adc8c422
Guilherme Thomas <gui.thomas@outlook.com>
2021-11-17 23:13:32 -0300

    Funcao nova de exportar dados

--- main.c
+++ main.c
@@ -26,12 +26,12 @@ struct paciente pac;
 void SetColor(int ForgC)
 {
      WORD wordcolor;
-     HANDLE studentHandleOutput = GetStdHandle(STD_OUTPUT_HANDLE);
+     HANDLE pacienteHandleOutput = GetStdHandle(STD_OUTPUT_HANDLE);
      CONSOLE_SCREEN_BUFFER_INFO conscrbufinfo;
-     if(GetConsoleScreenBufferInfo(studentHandleOutput, &conscrbufinfo))
+     if(GetConsoleScreenBufferInfo(pacienteHandleOutput, &conscrbufinfo))
      {
           wordcolor = (conscrbufinfo.wAttributes & 0xF0) + (ForgC & 0x0F);
-          SetConsoleTextAttribute(studentHandleOutput, wordcolor);
+          SetConsoleTextAttribute(pacienteHandleOutput, wordcolor);
      }
      return;
 }
@@ -40,7 +40,7 @@ void ClearConsoleToColors(int ForgC, int BackC)
 {
      WORD wordColor = ((BackC & 0x0F) << 4) + (ForgC & 0x0F);
 
-     HANDLE studentHandleOutput = GetStdHandle(STD_OUTPUT_HANDLE);
+     HANDLE pacienteHandleOutput = GetStdHandle(STD_OUTPUT_HANDLE);
 
      COORD coordinates = {0, 0};
 
@@ -48,14 +48,15 @@ void ClearConsoleToColors(int ForgC, int BackC)
 
      CONSOLE_SCREEN_BUFFER_INFO conscrbufinfo;
 
-     SetConsoleTextAttribute(studentHandleOutput, wordColor);
-     if(GetConsoleScreenBufferInfo(studentHandleOutput, &conscrbufinfo))
+     SetConsoleTextAttribute(pacienteHandleOutput, wordColor);
+     if(GetConsoleScreenBufferInfo(pacienteHandleOutput, &conscrbufinfo))
+
      {
 
-          FillConsoleOutputCharacter(studentHandleOutput, (TCHAR) 32, conscrbufinfo.dwSize.X * conscrbufinfo.dwSize.Y, coordinates, &counting);
-          FillConsoleOutputAttribute(studentHandleOutput, conscrbufinfo.wAttributes, conscrbufinfo.dwSize.X * conscrbufinfo.dwSize.Y, coordinates, &counting );
+          FillConsoleOutputCharacter(pacienteHandleOutput, (TCHAR) 32, conscrbufinfo.dwSize.X * conscrbufinfo.dwSize.Y, coordinates, &counting);
+          FillConsoleOutputAttribute(pacienteHandleOutput, conscrbufinfo.wAttributes, conscrbufinfo.dwSize.X * conscrbufinfo.dwSize.Y, coordinates, &counting );
 
-          SetConsoleCursorPosition(studentHandleOutput, coordinates);
+          SetConsoleCursorPosition(pacienteHandleOutput, coordinates);
      }
      return;
 }
@@ -68,73 +69,112 @@ void Setting_Color_And_Background(int ForgC, int BackC)
 }
 
 COORD coordinates = {0,0};
-void x_and_y_coordinates(int x, int y){
+void x_and_y_coordinates(int x, int y)
+
+{
     coordinates.X = x; coordinates.Y = y;
     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coordinates);
 }
 
-void Drawing_Rectangle(){
+void Drawing_Rectangle()
+{
     int a, b;
     x_and_y_coordinates(0,0);
     printf("%c",201);
-    for(a = 1; a < 78; a++){
+
+    for(a = 1; a < 78; a++)
+     {
         x_and_y_coordinates(a, 0);
         printf("%c",205);
-    }
+     }
+
     x_and_y_coordinates(78,0);
     printf("%c",187);
-    for(a = 1; a < 25; a++){
+
+    for(a = 1; a < 25; a++)
+
+      {
         x_and_y_coordinates(78, a);
-        if(a == 6){
+        if(a == 6)
+          {
             printf("%c",185);
-        }else{
+          }
+        else
+          {
             printf("%c",186);
-        }
-    }
+          }
+      }
     x_and_y_coordinates(78, 25);
     printf("%c",188);
-    for(a = 77; a > 0; a--){
+
+    for(a = 77; a > 0; a--)
+
+    {
         x_and_y_coordinates(a,25);
-        if(a == 35){
+        if(a == 35)
+          {
             printf("%c",202);
-        }else{
+          }
+
+        else
+
+          {
             printf("%c",205);
-        }
+          }
     }
+
     x_and_y_coordinates(0,25);
     printf("%c",200);
-    for(a = 24; a > 0; a--){
+
+    for(a = 24; a > 0; a--)
+    {
         x_and_y_coordinates(0,a);
-        if(a == 6){
+        if(a == 6)
+          {
             printf("%c",204);
-        }else{
+          }
+        else
+           {
             printf("%c",186);
-        }
+           }
     }
 
-    for(a = 1; a < 78; a++){
+    for(a = 1; a < 78; a++)
+
+        {
         x_and_y_coordinates(a,6);
-        if(a == 35){
+        if(a == 35)
+
+          {
             printf("%c",203);
-        }else{
+          }
+    else
+          {
             printf("%c",205);
-        }
+          }
     }
 
-    for(a = 7; a < 25; a++){
+    for(a = 7; a < 25; a++)
+
+      {
         x_and_y_coordinates(35,a);
         printf("%c",186);
-    }
+      }
 
 }
 
-void Reset_Window(){
+void Reset_Window()
+{
     int a,b;
-    for(a = 37; a < 78; a++){
-        for(b = 7; b < 25; b++){
+    for(a = 37; a < 78; a++)
+    {
+        for(b = 7; b < 25; b++)
+
+        {
             x_and_y_coordinates(a,b);printf(" ");
         }
     }
+
     return;
 }
 
@@ -243,6 +283,7 @@ void pesquisa_paciente()
             break;
          }
      }
+
     if(isFound == 1)
 
       {
@@ -258,7 +299,7 @@ void pesquisa_paciente()
         x_and_y_coordinates(37,17);printf("Idade : %s",pac.nascimento);
         x_and_y_coordinates(37,18);printf("Email: %s",pac.email);
         x_and_y_coordinates(37,19);printf("Data do Diagnostico: %s",pac.diagnostico);
-        x_and_y_coordinates(37,20);printf("Comorbidade: %s",pac.comorbidade);
+        x_and_y_coordinates(37,20);printf("Com  orbidade: %s",pac.comorbidade);
 
       }
 
@@ -273,43 +314,64 @@ void pesquisa_paciente()
     return;
 }
 
-/*
-void modify_student()
+
+void exporta_dados()
 {
     Reset_Window();
-    print_heading("Modify Record");
+
+    print_heading("Exportar");
     SetColor(45);
-    char student_id[15];
+    int ano = 2021;
+
+    char cpf[15];
     int isFound = 0, print = 37;
-    x_and_y_coordinates(37,10);printf("Enter ID to Modify: ");fflush(stdin);
-    gets(student_id);
+
+    x_and_y_coordinates(37,10);printf("CPF para exportar: ");fflush(stdin);
+
+    gets(cpf);
     FILE *openfile;
-    openfile = fopen("record.txt","rb+");
-    while(fread(&stu, sizeof(stu),1,openfile) == 1){
-        if(strcmp(student_id, stu.stud_id) == 0){
+    openfile = fopen("registros.txt","rb+");
+
+    while(fread(&pac, sizeof(pac),1,openfile) == 1)
+    {
+        if(strcmp(cpf, pac.cpf) == 0)
+         {
             fflush(stdin);
-            x_and_y_coordinates(print,12);printf("ID: ");gets(stu.stud_id);
-            x_and_y_coordinates(print,13);printf("Name: ");gets(stu.stud_name);
-            x_and_y_coordinates(print,14);printf("Address: ");gets(stu.stud_address);
-            x_and_y_coordinates(print,15);printf("Parent's name: ");gets(stu.stud_parentName);
-            x_and_y_coordinates(print,16);printf("Class: ");scanf("%d",&stu.stud_class);
-            x_and_y_coordinates(print,17);printf("Contact Number: ");scanf("%ld",&stu.stud_contact_number);
-            fseek(openfile,-sizeof(stu), SEEK_CUR);
-            fwrite(&stu,sizeof(stu), 1, openfile);
+            x_and_y_coordinates(print,11);printf("Idade do paciente: %d", pac.nascimento - ano);
+
+            x_and_y_coordinates(print,12);printf("ID: ");gets(pac.cpf);
+            x_and_y_coordinates(print,13);printf("Name: ");gets(pac.cpf);
+            x_and_y_coordinates(print,14);printf("Address: ");gets(pac.cpf);
+            x_and_y_coordinates(print,15);printf("Parent's name: ");gets(pac.cpf);
+            x_and_y_coordinates(print,16);printf("Class: ");scanf("%d",&pac.cpf);
+            x_and_y_coordinates(print,17);printf("Contact Number: ");scanf("%ld",&pac.cpf);
+
+            fseek(openfile,-sizeof(pac), SEEK_CUR);
+            fwrite(&pac,sizeof(pac), 1, openfile);
+
             x_and_y_coordinates(40,22); printf("New Student is Updated Successfully");
+
             isFound = 1;
+
             break;
-        }
+         }
     }
-    if(!isFound){
+
+    if(!isFound)
+
+    {
         x_and_y_coordinates(print, 12);printf("No Record Found");
     }
+
     fclose(openfile);
     SetColor(28);
     return;
 }
 
-*/
+
+
+
+
 
 void main_window()
   {
@@ -336,7 +398,7 @@ void main_window()
                 pesquisa_paciente();
                 break;
             case 3:
-                pesquisa_paciente();
+                exporta_dados();
                 break;
             case 4:
                 exit(0);
